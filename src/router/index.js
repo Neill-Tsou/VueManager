cc335bca5b9d09232e32f840ed5d4222947c45ba
Neill-Tsou <1317797708@qq.com>
2022-07-28 11:15:03 +0800

    #5

--- src/router/index.js
+++ src/router/index.js
@@ -63,5 +63,10 @@ const router = new VueRouter({
 })
 
 
+const originalPush = VueRouter.prototype.push;
+
+VueRouter.prototype.push = function push(location) {
+    return originalPush.call(this, location).catch(err => err)
+}
 
 export default router
